1d4e49b245c7b0058cf7ebe5941c9f48c215a536
Ulises <ulimartinez96@gmail.com>
2017-06-27 15:47:47 -0600

    errased text message

--- android/HF---Test/app/src/main/java/com/udg/t4/antonio/hf___test/MainActivity.java
+++ android/HF---Test/app/src/main/java/com/udg/t4/antonio/hf___test/MainActivity.java
@@ -1,5 +1,8 @@
 package com.udg.t4.antonio.hf___test;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -10,29 +13,34 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
-
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
             switch (item.getItemId()) {
                 case R.id.navigation_Profile:
-                    mTextMessage.setText("Profile");
-                    
-                    return true;
+                    fragment = new ProfileFragment();
+                    break;
                 case R.id.navigation_CalorieC:
-                    mTextMessage.setText("Calorie Count");
-                    return true;
+                    fragment = new CalorieFragment();
+                    break;
                 case R.id.navigation_home:
-                    mTextMessage.setText("Home");
-                    return true;
+                    fragment = new HomeFragment();
+                    break;
                 case R.id.navigation_Ingredients:
-                    mTextMessage.setText("Ingredients");
-                    return true;
+                    fragment = new IngredientFragment();
+                    break;
                 case R.id.navigation_Recipes:
-                    mTextMessage.setText("Recipes");
-                    return true;
+                    fragment = new RecipeFragment();
+                    break;
+            }
+            if (fragment != null) {
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.content, fragment);
+                fragmentTransaction.commit();
             }
             return false;
         }
